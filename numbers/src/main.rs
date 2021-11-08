3f5ee88b14df35d70e24fc4bb7943770c945f938
roblesdotdev <robles.contactme@gmail.com>
2021-11-08 10:59:18 -0300

    adds comparing nums

--- numbers/src/main.rs
+++ numbers/src/main.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 fn main() {
   // infers
   let twenty = 20;
@@ -19,4 +21,32 @@ fn main() {
 
   // elements within array are indexed starting at 0
   println!("{:02}", forty_twos[0]);
+
+  // comparing
+  comparing_nums_with_as_convertion();
+  comparing_nums_safety();
+}
+
+fn comparing_nums_with_as_convertion() {
+  let a: i32 = 10;
+  let b: u16 = 100;
+
+  // Warning: use type casts will cause your program to
+  // behave unexpected.
+  if a < (b as i32) {
+    println!("Ten is less than one hundred.");
+  }
+}
+
+fn comparing_nums_safety() {
+  let a: i32 = 10;
+  let b: u16 = 100;
+
+  // try_into() returns a Result type that provides access to the
+  // conversion attempt.
+  let b_ = b.try_into().unwrap();
+
+  if a < b_ {
+    println!("Ten is less than one hundred.");
+  }
 }
